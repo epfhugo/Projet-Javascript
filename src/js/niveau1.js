cd72ee85779ebe6465652937ffe73ebe351d24b2
hugo111111111 <153503455+hugo111111111@users.noreply.github.com>
2024-03-04 18:11:17 +0100

    début création page accueil

--- src/js/niveau1.js
+++ src/js/niveau1.js
@@ -1,58 +1,54 @@
 import * as fct from "/src/js/fonctions.js";
 
 export default class niveau1 extends Phaser.Scene {
+
+
+
   // constructeur de la classe
   constructor() {
     super({
       key: "niveau1" //  ici on précise le nom de la classe en tant qu'identifiant
     });
   }
+
+
+
   preload() {
+    this.load.tilemapTiledJSON("carte_lune", "src/assets/carte_lune.json"); 
+    this.load.image("vaisseau", "src/assets/vaisseau_marche.png"); 
+    this.load.image("monstre", "src/assets/monstre_2.png");
   }
 
   create() {
-    fct.doNothing();
-    fct.doAlsoNothing();
+    const carte_lune = this.add.tilemap("carte_lune");
 
-    this.add.image(400, 300, "img_ciel");
-    this.groupe_plateformes = this.physics.add.staticGroup();
-    this.groupe_plateformes.create(200, 584, "img_plateforme");
-    this.groupe_plateformes.create(600, 584, "img_plateforme");
-    // ajout d'un texte distintcif  du niveau
-    this.add.text(400, 100, "Vous êtes dans le niveau 1", {
-      fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif',
-      fontSize: "22pt"
-    });
+    const Surface = carteDuNiveau.createLayer(
+      "Surface",
+      tileset
+    );
 
-    this.porte_retour = this.physics.add.staticSprite(100, 550, "img_porte1");
+    const Planete = carteDuNiveau.createLayer(
+      "Planete",
+      tileset
+    );
 
-    this.player = this.physics.add.sprite(100, 450, "img_perso");
-    this.player.refreshBody();
-    this.player.setBounce(0.2);
-    this.player.setCollideWorldBounds(true);
-    this.clavier = this.input.keyboard.createCursorKeys();
-    this.physics.add.collider(this.player, this.groupe_plateformes);
+    const Fond = carteDuNiveau.createLayer(
+      "Fond",
+      tileset
+    );
+
+    player = this.physics.add(vaisseau);
+
+    player.setCollideWorldBounds(true); 
+
+    player.setBounce(0.2);
+
+    
+
+    clavier = this.input.keyboard.createCursorKeys(); 
   }
 
   update() {
-    if (this.clavier.left.isDown) {
-      this.player.setVelocityX(-160);
-      this.player.anims.play("anim_tourne_gauche", true);
-    } else if (this.clavier.right.isDown) {
-      this.player.setVelocityX(160);
-      this.player.anims.play("anim_tourne_droite", true);
-    } else {
-      this.player.setVelocityX(0);
-      this.player.anims.play("anim_face");
-    }
-    if (this.clavier.up.isDown && this.player.body.touching.down) {
-      this.player.setVelocityY(-330);
-    }
-
-    if (Phaser.Input.Keyboard.JustDown(this.clavier.space) == true) {
-      if (this.physics.overlap(this.player, this.porte_retour)) {
-        this.scene.switch("selection");
-      }
-    }
+    
   }
 }
