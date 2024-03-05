01d1cb81da368fe33562dba7945de6c61e77bf00
Lucas <lucas.leonardi@epfedu.fr>
2024-03-05 15:36:24 +0100

    Map Niveau 1 et 3 terminées

--- src/js/niveau1.js
+++ src/js/niveau1.js
@@ -1,7 +1,21 @@
 import * as fct from "/src/js/fonctions.js";
 
+var groupeBullets;
 var clavier;
-var Surface;
+
+function tirer(player) {
+  var coefDir;
+if (player.direction == 'left') { coefDir = -1; } else { coefDir = 1 }
+  // on crée la balle a coté du joueur
+  var bullet = groupeBullets.create(player.x + (25 * coefDir), player.y - 4, 'bullet');
+  // parametres physiques de la balle.
+  bullet.setCollideWorldBounds(true);
+  // on acive la détection de l'evenement "collision au bornes"
+  bullet.body.onWorldBounds = true;   
+  bullet.body.allowGravity =false;
+  bullet.setVelocity(1000 * coefDir, 0); // vitesse en x et en y
+}   
+
 export default class niveau1 extends Phaser.Scene {
 
   // constructeur de la classe
@@ -22,35 +36,35 @@ export default class niveau1 extends Phaser.Scene {
  
   create() {
 
-      const carte_lune = this.add.tilemap("carte_terre");
-      const tileset = carte_lune.addTilesetImage("Tile_NiveauTerre","Tuiles_Terre");
+      const carte_terre = this.add.tilemap("carte_terre");
+      const tileset = carte_terre.addTilesetImage("Tile_NiveauTerre","Tuiles_Terre");
   
       
-      const Fond = carte_lune.createLayer(
+      const Fond = carte_terre.createLayer(
         "Fond",
         tileset
       );
   
-      const Planete = carte_lune.createLayer(
+      const Planete = carte_terre.createLayer(
         "Planete",
         tileset
       );
       
-      const Surface = carte_lune.createLayer(
+      const Surface = carte_terre.createLayer(
         "Surface",
         tileset
       );
 
-      const autre = carte_lune.createLayer(
+      const autre = carte_terre.createLayer(
         "autre",
         tileset
       );
 
-      const Ville = carte_lune.createLayer(
+      const Ville = carte_terre.createLayer(
         " Ville",
         tileset
       );
-  
+
       this.player = this.physics.add.image(100, 450,"vaisseau_marche");
       this.player.setCollideWorldBounds(true); 
       this.player.setBounce(0.2);
@@ -61,33 +75,54 @@ export default class niveau1 extends Phaser.Scene {
       this.cameras.main.setBounds(0, 0, 6400, 610);
       // ancrage de la caméra sur le joueur
       this.cameras.main.startFollow(this.player);
-      
-      clavier = this.input.keyboard.createCursorKeys(); 
-    }
   
+      // creation d'un attribut direction pour le joueur, initialisée avec 'right'
+      this.player.direction = 'right'; 
+  
+      groupeBullets = this.physics.add.group();
+  
+      this.clavier = this.input.keyboard.createCursorKeys(); 
+  
+      // affectation de la touche A à boutonFeu
+      this.boutonFeu = this.input.keyboard.addKey('A'); 
+  
+      // instructions pour les objets surveillés en bord de monde
+      this.physics.world.on("worldbounds", function(body) {
+      // on récupère l'objet surveillé
+      var objet = body.gameObject;
+      // s'il s'agit d'une balle
+      if (groupeBullets.contains(objet)) {
+          // on le détruit
+          objet.destroy();
+      }
+    });
+  }
+
 
   update() {
  
-    if (clavier.left.isDown) {
+    if (this.clavier.left.isDown) {
       this.player.setVelocityX(-300);
       this.player.setTexture("vaisseau_recule");
+      this.player.direction = 'left'
 
-    } else if (clavier.right.isDown) {
+    } else if (this.clavier.right.isDown) {
       this.player.setVelocityX(300);
       this.player.setTexture("vaisseau_marche");
+      this.player.direction = 'right'
 
     } else {
       this.player.setVelocityX(0);
     }
 
-    if (clavier.up.isDown) {
+    if (this.clavier.up.isDown) {
       this.player.setVelocityY(-300);
       if (this.player.texture.key === "vaisseau_marche") {
           this.player.setTexture("vaisseau_haut");
       } else if (this.player.texture.key === "vaisseau_recule") {
           this.player.setTexture("vaisseau_bas_gauche");
       }
-  } else if (clavier.down.isDown) {
+  } else if (this.clavier.down.isDown) {
       this.player.setVelocityY(300);
       if (this.player.texture.key === "vaisseau_marche") {
           this.player.setTexture("vaisseau_bas");
@@ -102,9 +137,11 @@ export default class niveau1 extends Phaser.Scene {
       } else if (this.player.texture.key === "vaisseau_haut_gauche" || this.player.texture.key === "vaisseau_bas_gauche") {
           this.player.setTexture("vaisseau_recule");
       }
+  } 
+
+  if ( Phaser.Input.Keyboard.JustDown(this.boutonFeu)) {
+    tirer(this.player);
   }
-
-
     if (Phaser.Input.Keyboard.JustDown(clavier.space)) {
       if (this.physics.overlap(this.player, this.chiffre1))
         this.scene.switch("niveau1");
--- src/js/niveau3.js
+++ src/js/niveau3.js
@@ -1,3 +1,17 @@
+var groupeBullets;
+
+function tirer(player) {
+  var coefDir;
+if (player.direction == 'left') { coefDir = -1; } else { coefDir = 1 }
+  // on crée la balle a coté du joueur
+  var bullet = groupeBullets.create(player.x + (25 * coefDir), player.y - 4, 'bullet');
+  // parametres physiques de la balle.
+  bullet.setCollideWorldBounds(true);
+  // on acive la détection de l'evenement "collision au bornes"
+  bullet.body.onWorldBounds = true;   
+  bullet.body.allowGravity =false;
+  bullet.setVelocity(1000 * coefDir, 0); // vitesse en x et en y
+}   
 
 export default class niveau3 extends Phaser.Scene {
   // constructeur de la classe
@@ -6,49 +20,115 @@ export default class niveau3 extends Phaser.Scene {
       key: "niveau3" //  ici on précise le nom de la classe en tant qu'identifiant
     });
   }
-  preload() {}
+  preload() {
+
+    this.load.image("vaisseau_marche", "src/assets/vaisseau_marche.png"); 
+    this.load.image("Tuiles_Mars", "src/assets/Tile_NinveauMars.png");
+    this.load.tilemapTiledJSON("carte_mars", "src/assets/carte_mars.json"); 
+  }
 
   create() {
-    this.add.image(400, 300, "img_ciel");
-    this.groupe_plateformes = this.physics.add.staticGroup();
-    this.groupe_plateformes.create(200, 584, "img_plateforme");
-    this.groupe_plateformes.create(600, 584, "img_plateforme");
-    // ajout d'un texte distintcif  du niveau
-    this.add.text(400, 100, "Vous êtes dans le niveau 3", {
-      fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif',
-      fontSize: "22pt"
-    });
 
-    this.porte_retour = this.physics.add.staticSprite(100, 550, "img_porte3");
+    const carte_mars = this.add.tilemap("carte_mars");
+    const tileset = carte_mars.addTilesetImage("Tile_NinveauMars",'Tuiles_Mars');
 
-    this.player = this.physics.add.sprite(100, 450, "img_perso");
-    this.player.refreshBody();
+    
+    const Fond = carte_mars.createLayer(
+      "Fond",
+      tileset
+    );
+
+    const autre = carte_mars.createLayer(
+      "autre",
+      tileset
+    );
+
+    const Surface = carte_mars.createLayer(
+      "Surface",
+      tileset
+    );
+
+
+    const Planete = carte_mars.createLayer(
+      "Planete",
+      tileset
+    );
+    
+    this.player = this.physics.add.image(100, 450,"vaisseau_marche");
+    this.player.setCollideWorldBounds(true); 
     this.player.setBounce(0.2);
-    this.player.setCollideWorldBounds(true);
-    this.clavier = this.input.keyboard.createCursorKeys();
-    this.physics.add.collider(this.player, this.groupe_plateformes);
-  }
+
+    // redimensionnement du monde avec les dimensions calculées via tiled
+    this.physics.world.setBounds(0, 0, 6400, 610);
+    // ajout du champs de la caméra de taille identique à celle du monde
+    this.cameras.main.setBounds(0, 0, 6400, 610);
+    // ancrage de la caméra sur le joueur
+    this.cameras.main.startFollow(this.player);
+
+    // creation d'un attribut direction pour le joueur, initialisée avec 'right'
+    this.player.direction = 'right'; 
+
+    groupeBullets = this.physics.add.group();
+
+    this.clavier = this.input.keyboard.createCursorKeys(); 
+
+    // affectation de la touche A à boutonFeu
+    this.boutonFeu = this.input.keyboard.addKey('A'); 
+
+    // instructions pour les objets surveillés en bord de monde
+    this.physics.world.on("worldbounds", function(body) {
+    // on récupère l'objet surveillé
+    var objet = body.gameObject;
+    // s'il s'agit d'une balle
+    if (groupeBullets.contains(objet)) {
+        // on le détruit
+        objet.destroy();
+    }
+  });
+}
 
   update() {
+
     if (this.clavier.left.isDown) {
-      this.player.setVelocityX(-160);
-      this.player.anims.play("anim_tourne_gauche", true);
+      this.player.setVelocityX(-300);
+      this.player.setTexture("vaisseau_recule");
+      this.player.direction = 'left'
+
     } else if (this.clavier.right.isDown) {
-      this.player.setVelocityX(160);
-      this.player.anims.play("anim_tourne_droite", true);
+      this.player.setVelocityX(300);
+      this.player.setTexture("vaisseau_marche");
+      this.player.direction = 'right'
+
     } else {
       this.player.setVelocityX(0);
-      this.player.anims.play("anim_face");
-    }
-    if (this.clavier.up.isDown && this.player.body.touching.down) {
-      this.player.setVelocityY(-330);
     }
 
-    if (Phaser.Input.Keyboard.JustDown(this.clavier.space) == true) {
-      if (this.physics.overlap(this.player, this.porte_retour)) {
-        console.log("niveau 3 : retour vers selection");
-        this.scene.switch("selection");
+    if (this.clavier.up.isDown) {
+      this.player.setVelocityY(-300);
+      if (this.player.texture.key === "vaisseau_marche") {
+          this.player.setTexture("vaisseau_haut");
+      } else if (this.player.texture.key === "vaisseau_recule") {
+          this.player.setTexture("vaisseau_bas_gauche");
       }
-    }
+  } else if (this.clavier.down.isDown) {
+      this.player.setVelocityY(300);
+      if (this.player.texture.key === "vaisseau_marche") {
+          this.player.setTexture("vaisseau_bas");
+      } else if (this.player.texture.key === "vaisseau_recule") {
+          this.player.setTexture("vaisseau_haut_gauche");
+      }
+  } else {
+      // Réinitialisation de la vélocité verticale lorsque la touche du haut ou du bas n'est pas enfoncée
+      this.player.setVelocityY(0);
+      if (this.player.texture.key === "vaisseau_haut" || this.player.texture.key === "vaisseau_bas") {
+          this.player.setTexture("vaisseau_marche");
+      } else if (this.player.texture.key === "vaisseau_haut_gauche" || this.player.texture.key === "vaisseau_bas_gauche") {
+          this.player.setTexture("vaisseau_recule");
+      }
+  } 
+
+  if ( Phaser.Input.Keyboard.JustDown(this.boutonFeu)) {
+    tirer(this.player);
   }
+}
 }
